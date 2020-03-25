b637361591a9f94efc3fe4ae12f00c1cf5029082
Aslan <berezg86@gmail.com>
2020-03-25 21:36:54 +0300

    Страница хочу

--- gulp/tasks/serve.js
+++ gulp/tasks/serve.js
@@ -3,7 +3,7 @@ module.exports = function() {
         $.bs.init({
             server: {
                 baseDir: "./build",
-                index: "contact.html"
+                index: "want.html"
             }
         });
     });
--- src/static/js/common.js
+++ src/static/js/common.js
@@ -1,3 +1,5 @@
 $(document).ready(function(){
-
+    $('.tooltip__delete').click(function(){
+        $(this).parents('li').remove();
+    });
 });

8ba74c706e64e8214f7aba8f8f8a88ea1c2535fc
Aslan <berezg86@gmail.com>
2020-03-26 00:43:47 +0300

    Страница найти именинника

--- gulp/tasks/serve.js
+++ gulp/tasks/serve.js
@@ -3,7 +3,7 @@ module.exports = function() {
         $.bs.init({
             server: {
                 baseDir: "./build",
-                index: "want.html"
+                index: "search.html"
             }
         });
     });
--- src/static/js/common.js
+++ src/static/js/common.js
@@ -2,4 +2,5 @@ $(document).ready(function(){
     $('.tooltip__delete').click(function(){
         $(this).parents('li').remove();
     });
+    $(".inp_phone").mask("+7(999)999-99-99");
 });

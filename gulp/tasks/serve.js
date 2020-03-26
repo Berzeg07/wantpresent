d8cbd528873fa74d3b116d826d958866f6b862fe
Aslan <berezg86@gmail.com>
2020-03-26 09:51:10 +0300

    Страница результаты поиска

--- gulp/tasks/serve.js
+++ gulp/tasks/serve.js
@@ -3,7 +3,7 @@ module.exports = function() {
         $.bs.init({
             server: {
                 baseDir: "./build",
-                index: "search.html"
+                index: "search-result.html"
             }
         });
     });

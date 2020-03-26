335f0703118570eb13e9f51f8dd5c1e512f5d8fc
Aslan <berezg86@gmail.com>
2020-03-26 14:18:53 +0300

    Страницы телефон и смс

--- gulp/tasks/serve.js
+++ gulp/tasks/serve.js
@@ -3,7 +3,7 @@ module.exports = function() {
         $.bs.init({
             server: {
                 baseDir: "./build",
-                index: "payment-passed.html"
+                index: "sms.html"
             }
         });
     });

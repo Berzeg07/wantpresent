8fc2db737ecb54eacffeba099fb69b659594e0a0
Aslan <berezg86@gmail.com>
2020-03-26 12:22:09 +0300

    Страница профиль

--- gulp/tasks/serve.js
+++ gulp/tasks/serve.js
@@ -3,7 +3,7 @@ module.exports = function() {
         $.bs.init({
             server: {
                 baseDir: "./build",
-                index: "search-result.html"
+                index: "profile.html"
             }
         });
     });
